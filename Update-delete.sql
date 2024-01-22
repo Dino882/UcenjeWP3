47e18fce60f624a9032f9234fe934a0ddac2812d
Dino882 <dino.sabljic@gmail.com>
2024-01-22 23:36:15 +0100

    Ponavljanje sintakse
    Ponovljena sintaksa update i delete

--- Update-delete.sql
+++ Update-delete.sql
@@ -1,55 +1,33 @@
-use edunovawp3;
-select * from smjerovi 
-where sifra=1;
+select * from smjerovi
+where sifra =1;
 
+update smjerovi set naziv = 'C# programiranje'
+where sifra = 1;
 
-update smjerovi set naziv='C# programiranje'
-where sifra=1;
+update smjerovi set trajanje = 300, cijena = 2000
+where sifra = 1;
 
+update smjerovi set naziv = 'Serviser', trajanje= 120, cijena= 15000
+where sifra = 1;
 
-update smjerovi set trajanje=300, cijena=2000
-where sifra=2;
-
-
-update smjerovi set naziv='Serviser', trajanje=120, cijena=1500
-where sifra=3;
-
-
-select * from smjerovi;
-
-
-
-
-update smjerovi set cijena=cijena*1.2;
-
-
-
-update smjerovi set cijena=cijena*0.8;
-
-
-
-update smjerovi set cijena=cijena-100;
+update smjerovi set cijena = cijena*1.2;
+update smjerovi set cijena = cijena*0.8;
+update smjerovi set cijena = cijena-100;
 
 select * from polaznici;
 
-
-
-update polaznici set oib='00000000001', brojugovora='12/2023'
-where sifra=18;
+update polaznici set oib = '00000000001', brojugovora = '12/2023'
+where sifra = 18;
 
 update polaznici set oib='     ', brojugovora=''
 where sifra=19;
 
-update polaznici set oib=null, brojugovora=null
+update polaznici set oib = null, brojugovora = null
 where sifra=19;
 
 
-
-
-
-
-
---select *from smjerovi
---delete from clanovi;
---delete from grupe;
---delete  from smjerovi;
+select * from smjerovi;
+delete from smjerovi where sifra = 1;
+delete from clanovi;
+delete from grupe;
+delete from smjerovi;
