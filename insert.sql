9b0a027aff9b57e4633a0b92f5d62b9c3f0bd7b4
Dino882 <dino.sabljic@gmail.com>
2024-01-22 23:05:48 +0100

    Ponavljanje sintakse
    Ponovljena sintaksa insert

--- insert.sql
+++ insert.sql
@@ -1,13 +1,39 @@
 use edunovawp3;
-
 select * from smjerovi;
+insert into smjerovi (naziv,trajanje,cijena,vaucer)
+values ('Web programiranje', 225,1859.45,1);
 
-insert into smjerovi (naziv, trajanje, cijena, vaucer)
-values ('Web programiranje', 225, 1859.45,1);
+select * from predavaci;
+insert into predavaci (ime,prezime,email)
+values 
+('Tomislav', 'Jakopec','tjakopec@gmail.com'),
+('Shaquille', 'O''Neal','shaki@gmail,com');
 
--- loÅ¡a sintaksa
-insert into smjerovi values 
-('Web dizajn', null,null,null);
 
--- minimalna dobra sintaksa
-insert into smjerovi (naziv) values ('ÄÅ¡Ä‡Ä‘ ÄŒÅ Ä†Ä');
+select * from polaznici;
+insert into polaznici (ime,prezime,email) values
+('Slaven','Pozniæ','slaven.poznic@hotmail.com'),
+('Karla','Kraljik','kraljikkarla@gmail.com'),
+('Dino','Sabljiæ','dino.sabljic@gmail.com'),
+('Luka','Crljiæ','lukacrljic7@gmail.com'),
+('Mario','Baliban','mariobalibanos@gmail.com'),
+('Branimir','Fekete','branimir.fekete@gmail.com'),
+('Dajana','Toliæ','dajanabozic@gmail.com'),
+('Ivan','Toliæ','ivan.tolic1989@gmail.com'),
+('Tomislav','Kuèiæ','tomislavkucic@gmail.com'),
+('Zdenko','Bariæ','zbaric_hr@yahoo.com'),
+('Mihael','ulj','mihaelzulj@gmail.com'),
+('Ivan','Maroševiæ','ivanmarosevic4@gmail.com'),
+('Goran','Kovaèiæ','kovacic.go@gmail.com'),
+('Nataša','Jakopec','njakopec@gmail.com'),
+('Goran','Franjiæ','goran@proces.hr'),
+('Ana','Juriæ','ana.juric19@gmail.com'),
+('Emina','Vejsiloviæ','emiomiemi@gmail.com'),
+('Josip','Rebolj','rebolj.josip031@gmail.com'),
+('Merlin','Jišše','jisse.merlin@gmail.com'),
+('Sneana','Grkoviæ','snezana4688@yahoo.com'),
+('Rade','Jasenovèanin','rade.jasenovcanin@yahoo.com'),
+('Krešimir','Bilokapiæ','ilead7@gmail.com'),
+('Srğan','Filipoviæ','srdjanfilipovic991@gmail.com'),
+('Veljko','Vujiæ','veljko2805vujic@gmail.com');
+
