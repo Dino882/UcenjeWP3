0b6d473e8b5104642f5d272ce851e7f705eb07f2
Dino882 <dino.sabljic@gmail.com>
2024-02-04 21:23:03 +0100

    Provjera

---
+++ CSHARP/UcenjeWP3/WebAPI/Controllers/E01UlazIzlaz.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+
+    [ApiController]
+    [Route("E01")]
+    public class E01UlazIzlaz: ControllerBase
+    {
+
+        [HttpGet]
+        [Route("Hello")]
+
+        public string HelloWorld()
+        {
+            return "Hello";
+
+        }
+
+
+    }
+}
